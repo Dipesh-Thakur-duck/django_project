95ccaae61105e4b0c7f5cda3af7607ec0bfb5bed
Dipesh Thakur <letsmaildipesh1@gmail.com>
2025-07-20 17:33:12 +0545

    Add bootstrap in base template and use url tag
    Base template is filled with bootstrap code and
    url tag is used to make navigation bar responsive

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author':'Dipesh Thakur',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'August 27, 2018'
+    },
+    {
+        'author':'Nikita Rayamajhi',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'August 28, 2018'
+    }
+]
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request,'blog/about.html')
+    return render(request,'blog/about.html',{'title':'About'})
